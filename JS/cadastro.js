2545dad8ccbbfa8030689f71d91e0c43748447e5
Jonas Leite <jonas.programador01@gmail.com>
2023-10-09 14:26:55 -0300

    arrumar barra de pesquisa

--- JS/cadastro.js
+++ JS/cadastro.js
@@ -1,100 +1,103 @@
-const form = document.getElementById("cadastroForm");
+
+const appearsAnimal = document.querySelector('#result');
 
 
-function cadastrar(){
-    
-    const nomePet = document.getElementById("nome").value;
-    const idadePet = document.getElementById("idade").value;
+
+function registerPet() {
+
+    const yearsPet = document.getElementById("name").value;
+    const namePet = document.getElementById("years").value;
     const filesPet = document.getElementById("filesPet").files[0];
 
-    if (nomePet && idadePet && filesPet) {
-        const chave = `pet_${Date.now()}`;
-        const dadosPet = `Nome: ${nomePet}, Idade: ${idadePet} `;
-        
+    if (namePet && yearsPet && filesPet) {
+        const keyPet = `pet_${Date.now()}`;
+        const dataPet = `Nome: ${namePet}, Idade: ${yearsPet} `;
+
         const reader = new FileReader();
         reader.readAsDataURL(filesPet);
         reader.onload = function () {
             const imageBase64 = reader.result;
-            localStorage.setItem(chave, `${dadosPet}, Imagem: ${imageBase64}`);
+            localStorage.setItem(keyPet, `${dataPet}, Imagem: ${imageBase64}`);
         };
 
-        document.getElementById("nome").value = ""; 
-        document.getElementById("idade").value = "";
+        document.getElementById("name").value = "";
+        document.getElementById("years").value = "";
         document.getElementById("filesPet").value = "";
         window.location = "ListaCadastro.html";
     }
-   
+
 }
-const apareceDog = document.querySelector('#resultado');
-function carregarPets(){
-  
 
- 
-    apareceDog.innerHTML = ""; 
+function LoadingPets() {
 
-    for (let chave in localStorage) {
-        if (chave.startsWith('pet_')) {
-            const dadosPet = localStorage.getItem(chave);
-            const dadosPetArray = dadosPet.split(", ");
-            const nomePet = dadosPetArray[0].split(": ")[1];
-            const idadePet = dadosPetArray[1].split(": ")[1];
-            const imagemBase64 = dadosPetArray[2].split(": ")[1];
 
-            
-            const aparece = `<div class="animal">
-            <img src="${imagemBase64}" alt="Imagem do PET">
-            <span>Nome: ${nomePet}</span>
-            <span>Idade: ${idadePet}</span>
-            <button class="remover-item" data-chave="${chave}" onclick="removerPet(event)">Remover</button>
+
+    appearsAnimal.innerHTML = "";
+
+    for (let keyPet in localStorage) {
+        if (keyPet.startsWith('pet_')) {
+            const dataPet = localStorage.getItem(keyPet);
+            const dataPetArray = dataPet.split(", ");
+            const namePet = dataPetArray[0].split(": ")[1];
+            const yearsPet = dataPetArray[1].split(": ")[1];
+            const imageBase64 = dataPetArray[2].split(": ")[1];
+
+
+            const screen = `<div class="animal">
+            <img src="${imageBase64}" alt="Imagem do PET">
+            <span>Nome: ${namePet}</span>
+            <span>Idade: ${yearsPet}</span>
+            <button class="remover-item" data-chave="${keyPet}" onclick="removePet(event)">Remover</button>
         </div>`;
-            apareceDog.innerHTML += aparece;
+            appearsAnimal.innerHTML += screen;
         }
     }
 }
-    
- 
 
 
- 
- function removerPet(event) {
-    const chave = event.target.getAttribute("data-chave");
-    localStorage.removeItem(chave);
 
- 
-    carregarPets(); 
+
+
+function removePet(event) {
+    const key = event.target.getAttribute("data-chave");
+    localStorage.removeItem(key);
+
+
+    LoadingPets();
 }
-function adicionarExemplos() {
-    const exemplos = [
-        { nome: "Nebulosa", idade: "4 meses", imagem: "../Images/Carrocel2.jpg" },
-        { nome: "Luna", idade: "2 anos", imagem: "/Images/Carrocel3.jpg" },
-        { nome: "Blobby", idade: "2 anos", imagem: "/Images/pexels-umut-sarıalan-17657304.jpg" }
+ function addExamples() {
+    
+    const examples = [
+        { name: "Nebulosa", years: "4 meses", image: "./Images/Carrocel2.jpg" },
+        { name: "Luna", years: "2 anos", image: "/Images/Carrocel3.jpg" },
+        { name: "Blobby", years: "2 anos", image: "/Images/pexels-umut-sarıalan-17657304.jpg" }
     ];
 
-    exemplos.forEach((exemplo, index) => {
-        const chave = `pet_${Date.now() + index}`;
-        const dadosPet = `Nome: ${exemplo.nome}, Idade: ${exemplo.idade}, Imagem: ${exemplo.imagem}`;
-        
-        // Adicione a classe "dog" aos animais fictícios
-        const aparece = `<div class="animal">
-            <img src="${exemplo.imagem}" alt="Imagem do PET">
-            <span>Nome: ${exemplo.nome}</span>
-            <span>Idade: ${exemplo.idade}</span>
-            <button class="remover-item" data-chave="${chave}" onclick="removerPet(event)">Remover</button>
+    examples.forEach((example, index) => {
+        const keyPet = `pet_${Date.now() + index}`;
+        const dataPet = `Nome: ${example.name}, Idade: ${example.years}, Imagem: ${example.image}`;
+
+
+        const screen = `<div class="animal">
+            <img src="${example.name}" alt="Imagem do PET">
+            <span>Nome: ${example.years}</span>
+            <span>Idade: ${example.image}</span>
+            <button class="remover-item" data-chave="${keyPet}" onclick="removerPet(event)">Remover</button>
         </div>`;
-        apareceDog.innerHTML += aparece
-        localStorage.setItem(chave, dadosPet);
+        appearsAnimal.innerHTML += screen
+        localStorage.setItem(keyPet, dataPet);
     });
 }
 
 function searchPets() {
     const searchTerm = document.getElementById("search").value.toLowerCase();
-    const listaPets = document.querySelectorAll(".animal");
+    const listPets = document.querySelectorAll(".animal");
 
-    listaPets.forEach(pet => {
-        const petNome = pet.querySelector("span:nth-child(2)").textContent.toLowerCase();
-        const petIdade = pet.querySelector("span:nth-child(3)").textContent.toLowerCase();
+    listPets.forEach(pet => {
+        const petName = pet.querySelector("span:nth-child(2)").textContent.toLowerCase();
+        const petYears = pet.querySelector("span:nth-child(3)").textContent.toLowerCase();
 
-        if (petNome.includes(searchTerm) || petIdade.includes(searchTerm)) {
+        if (petName.includes(searchTerm) || petYears.includes(searchTerm)) {
             pet.style.display = "";
         } else {
             pet.style.display = "none";
@@ -102,16 +105,15 @@ function searchPets() {
     });
 
     const message = document.getElementById("message");
-    const resultado = document.getElementById("resultado");
+    const result = document.getElementById("result");
 
     if (searchTerm === "") {
         message.textContent = "";
-    } else if (resultado.children.length === 0) {
+    } else if (result.children.length === 0) {
         message.textContent = "Nenhum resultado encontrado.";
     } else {
         message.textContent = `Exibindo resultados para "${searchTerm}".`;
     }
 }
 
-adicionarExemplos();
-carregarPets();
+
